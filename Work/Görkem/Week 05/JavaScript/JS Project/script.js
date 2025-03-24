31b30bea8c6d903dc3d760ded896ea197622f2a5
Görkem Cinar <grcinar@outlook.com>
2025-03-24 11:16:30 +0100

    update script.js

--- Work/Görkem/Week 05/JavaScript/JS Project/script.js
+++ Work/Görkem/Week 05/JavaScript/JS Project/script.js
@@ -2,33 +2,33 @@
 let attempts = 0;
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-const guess = document.getElementById('guess');
-const submit = document.getElementById('submit');
-const hint = document.getElementById('hint');
-const attemptsText = document.getElementById('attempts');
+const guess = document.getElementById('guess');                                      // Holt das Eingabefeld mit der ID 'guess'
+const submit = document.getElementById('submit');                                   // Holt den Button mit der ID 'submit'
+const hint = document.getElementById('hint');                                      // Holt das Element mit der ID 'hint', in dem Hinweise wie "Too high!" angezeigt werden
+const attemptsText = document.getElementById('attempts');                         // Holt das Element mit der ID 'attempts', das die Anzahl der Versuche anzeigt
 //*console.log(guess, submit);*//
 
 // Event listeners
-submit.addEventListener('click', checkGuess);
+submit.addEventListener('click', checkGuess);                                    // Füge einen Event Listener hinzu, der auf den Klick des Buttons wartet
 
 function checkGuess() {
-    const userValue = Number (guess.value);
-    attempts++;
+    const userValue = Number (guess.value);                                      // Der Wert des Eingabefelds mit der ID 'guess' wird abgerufen und in eine Zahl umgewandelt
+    attempts++;                                                                 // Anzahl der Versuche erhöhen
 
     // If- Statment
-    if(userValue === randomNumber) {
-        hint.textContent = "🎉 Congratulations, you guessed it :-)";
+    if(userValue === randomNumber) {                                            // Überprüfen, ob die Eingabe mit der Zufallszahl übereinstimmt
+        hint.textContent = "🎉 Congratulations, you guessed it :-)";           // Falls richtig, Erfolgsmeldung anzeigen und Konfetti abfeuern
         fireConfetti();
-    } else if (userValue < randomNumber) {
+    } else if (userValue < randomNumber) {                                     // Falls die Eingabe kleiner als die Zufallszahl ist, Hinweis geben
         hint.textContent = "Too low! Try again.";
-    } else {
+    } else {                                                                   // Falls die Eingabe größer als die Zufallszahl ist, Hinweis geben
         hint.textContent = "Too high! Try again.";
     }
 
     attemptsText.textContent =
-    "Attempts: " + attempts;
+    "Attempts: " + attempts;                                                   // Die Anzahl der Versuche wo im HTML aktualisiert
 }
-
+// Konfetti function
 function fireConfetti() {
     confetti({
         particleCount: 150,
