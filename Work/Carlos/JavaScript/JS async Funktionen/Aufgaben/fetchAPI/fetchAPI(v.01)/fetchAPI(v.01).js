58c6ae6ae4c0ff9bb251761d5953f435e777e1eb
Carlos Artiaga <carlesam89@gmail.com>
2025-04-22 15:26:49 +0200

    Journal Update

---
+++ Work/Carlos/JavaScript/JS async Funktionen/Aufgaben/fetchAPI/fetchAPI(v.01)/fetchAPI(v.01).js
@@ -0,0 +1,25 @@
+async function fetchNextJoke(){
+    let result = await fetch('https://sv443.net/jokeapi/v2/joke/Programming?type=twopart')
+    if (result.ok) {
+        let content = await result.json()
+        showJoke(content)
+    } else {
+        console.log('Server is not ready to deliver a new joke')
+    }
+}
+
+function showJoke(joke){
+    const jokeElement = document.getElementById('joke')
+
+    const pSetup = document.createElement('p')
+    pSetup.classList.add('setup')
+    pSetup.textContent = joke.setup
+
+    const pDelivery = document.createElement('p')
+    pDelivery.classList.add('delivery')
+    pDelivery.textContent = joke.delivery
+
+    jokeElement.innerHTML = ''
+    jokeElement.appendChild(pSetup)
+    jokeElement.appendChild(pDelivery)
+}
