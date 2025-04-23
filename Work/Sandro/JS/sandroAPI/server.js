f0debdff01444d918afa374a04acf6818d672db1
ossdraboon <ossdraboon@gmail.com>
2025-04-23 21:08:02 +0200

    server and fetch update

--- Work/Sandro/JS/sandroAPI/server.js
+++ Work/Sandro/JS/sandroAPI/server.js
@@ -3,12 +3,115 @@ const app = express()
 const port = 3000
 
 let objects = [
-  { id: 1, name: 'Object 1' },
-  { id: 2, name: 'Object 2' },
-  { id: 3, name: 'Object 3' },
-  { id: 7, name: 'Object 7' },
+  {
+    "id": 1,
+    "name": "Google Pixel 6 Pro",
+    "data": {
+      "color": "Cloudy White",
+      "capacity": "128 GB"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Apple iPhone 12 Mini, 256GB, Blue",
+    "data": null
+  },
+  {
+    "id": 3,
+    "name": "Apple iPhone 12 Pro Max",
+    "data": {
+      "color": "Cloudy White",
+      "capacity GB": 512
+    }
+  },
+  {
+    "id": 4,
+    "name": "Apple iPhone 11, 64GB",
+    "data": {
+      "price": 389.99,
+      "color": "Purple"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Samsung Galaxy Z Fold2",
+    "data": {
+      "price": 689.99,
+      "color": "Brown"
+    }
+  },
+  {
+    "id": 6,
+    "name": "Apple AirPods",
+    "data": {
+      "generation": "3rd",
+      "price": 120
+    }
+  },
+  {
+    "id": 7,
+    "name": "Apple MacBook Pro 16",
+    "data": {
+      "year": 2019,
+      "price": 1849.99,
+      "CPU model": "Intel Core i9",
+      "Hard disk size": "1 TB"
+    }
+  },
+  {
+    "id": 8,
+    "name": "Apple Watch Series 8",
+    "data": {
+      "Strap Colour": "Elderberry",
+      "Case Size": "41mm"
+    }
+  },
+  {
+    "id": 9,
+    "name": "Beats Studio3 Wireless",
+    "data": {
+      "Color": "Red",
+      "Description": "High-performance wireless noise cancelling headphones"
+    }
+  },
+  {
+    "id": 10,
+    "name": "Apple iPad Mini 5th Gen",
+    "data": {
+      "Capacity": "64 GB",
+      "Screen size": 7.9
+    }
+  },
+  {
+    "id": 11,
+    "name": "Apple iPad Mini 5th Gen",
+    "data": {
+      "Capacity": "254 GB",
+      "Screen size": 7.9
+    }
+  },
+  {
+    "id": 12,
+    "name": "Apple iPad Air",
+    "data": {
+      "Generation": "4th",
+      "Price": "419.99",
+      "Capacity": "64 GB"
+    }
+  },
+  {
+    "id": 13,
+    "name": "Apple iPad Air",
+    "data": {
+      "Generation": "4th",
+      "Price": "519.99",
+      "Capacity": "256 GB"
+    }
+  }
 ]
-let lastId = 7
+
+
+let lastId = objects.reduce((max, obj) => Math.max(max, obj.id), 0)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -66,7 +169,7 @@ app.patch('/objects/:id', (req, res) => {
   }
   if (req.body.data) {
     if (!object.data) {
-        object.data = {}
+      object.data = {}
     }
     for (const key in req.body.data) {
       if (req.body.data.hasOwnProperty(key)) {
