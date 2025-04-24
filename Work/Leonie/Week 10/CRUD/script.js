3172c456edfe53cdcbea7df2c15a9f12fdc5ce24
Leonie Ertl <ertl.leonie2004@gmail.com>
2025-04-24 09:56:53 +0200

    eigene api probiert

---
+++ Work/Leonie/Week 10/CRUD/script.js
@@ -0,0 +1,45 @@
+addEventListener('DOMContentLoaded', () => {
+    getData()
+})
+
+
+async function getData() {
+    const result = await fetch('https://api.restful-api.dev/objects')
+    const data = await result.json()
+    if (result.ok) {
+        console.log(data)
+        createDOM(data)
+    } else {
+        alert("Error: " + result.status + " " + result.statusText);
+        return null
+    }
+}
+
+function createDOM(data) {
+    const container = document.querySelector('.content');
+    container.innerHTML = '';
+    data.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('item');
+
+        const aLink = document.createElement('a');
+        aLink.href = `./details.html?id=${item.id}`;
+        aLink.textContent = item.name;
+
+        // Event Listener für den Link
+        aLink.addEventListener('click', (event) => {
+            event.preventDefault(); // Verhindert das Standardverhalten des Links
+            console.log(`Link clicked for item: ${item.name}`);
+            
+            // Hier kannst du weitere Aktionen ausführen
+        });
+
+        const button = document.createElement('button');
+        button.textContent = 'Delete';
+
+        container.appendChild(div);
+        div.appendChild(aLink);
+        div.appendChild(button);
+    });
+}
+
