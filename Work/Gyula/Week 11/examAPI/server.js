447412adc571779fe47780a4bd48df93c17dca67
Gyula Horvath <gylhrvth@gmail.com>
2025-04-28 10:11:24 +0200

    Update Server

--- Work/Gyula/Week 11/examAPI/server.js
+++ Work/Gyula/Week 11/examAPI/server.js
@@ -9,109 +9,15 @@ let users = [
 
 let todos = [
   {
-    "id": 1,
-    "name": "Google Pixel 6 Pro",
-    "data": {
-      "color": "Cloudy White",
-      "capacity": "128 GB"
-    }
-  },
-  {
-    "id": 2,
-    "name": "Apple iPhone 12 Mini, 256GB, Blue",
-    "data": null
-  },
-  {
-    "id": 3,
-    "name": "Apple iPhone 12 Pro Max",
-    "data": {
-      "color": "Cloudy White",
-      "capacity GB": 512
-    }
-  },
-  {
-    "id": 4,
-    "name": "Apple iPhone 11, 64GB",
-    "data": {
-      "price": 389.99,
-      "color": "Purple"
-    }
-  },
-  {
-    "id": 5,
-    "name": "Samsung Galaxy Z Fold2",
-    "data": {
-      "price": 689.99,
-      "color": "Brown"
-    }
-  },
-  {
-    "id": 6,
-    "name": "Apple AirPods",
-    "data": {
-      "generation": "3rd",
-      "price": 120
-    }
-  },
-  {
-    "id": 7,
-    "name": "Apple MacBook Pro 16",
-    "data": {
-      "year": 2019,
-      "price": 1849.99,
-      "CPU model": "Intel Core i9",
-      "Hard disk size": "1 TB"
-    }
-  },
-  {
-    "id": 8,
-    "name": "Apple Watch Series 8",
-    "data": {
-      "Strap Colour": "Elderberry",
-      "Case Size": "41mm"
-    }
-  },
-  {
-    "id": 9,
-    "name": "Beats Studio3 Wireless",
-    "data": {
-      "Color": "Red",
-      "Description": "High-performance wireless noise cancelling headphones"
-    }
-  },
-  {
-    "id": 10,
-    "name": "Apple iPad Mini 5th Gen",
-    "data": {
-      "Capacity": "64 GB",
-      "Screen size": 7.9
-    }
-  },
-  {
-    "id": 11,
-    "name": "Apple iPad Mini 5th Gen",
-    "data": {
-      "Capacity": "254 GB",
-      "Screen size": 7.9
-    }
-  },
-  {
-    "id": 12,
-    "name": "Apple iPad Air",
-    "data": {
-      "Generation": "4th",
-      "Price": "419.99",
-      "Capacity": "64 GB"
-    }
-  },
-  {
-    "id": 13,
-    "name": "Apple iPad Air",
-    "data": {
-      "Generation": "4th",
-      "Price": "519.99",
-      "Capacity": "256 GB"
-    }
+    id: '123-456-232312313123131313',
+    title: 'Test',
+    description: 'Test Description',
+    dueDate: '2023-10-01',
+    complete: false,
+    responsible: 'Daniela',
+    createdBy: 'Gyula',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }
 ]
 
@@ -130,24 +36,40 @@ app.use(cors({
 
 
 app.get('/api/todo', (req, res) => {
-  res.json(todos)
+  res.json(todos.map(todo => {
+    return {
+      id: todo.id,
+      title: todo.title,
+      complete: todo.complete,
+    }
+  }
+  ))
 })
 
 app.get('/api/todo/:id', (req, res) => {
-  const todo = todos.find(obj => obj.id === parseInt(req.params.id))
+  const todo = todos.find(obj => obj.id === req.params.id)
   if (!todo) return res.status(404).send('The TODO with the given ID was not found.')
   res.json(todo)
 })
 
 app.post('/api/todo', (req, res) => {
-  if (Date.parse(req.body.dueDate) === NaN) {
+  if (!req.body.title) {
+    return res.status(400).send('Fehlende Angabe: title')
+  }
+  if (req.body.dueDate && Date.parse(req.body.dueDate) === NaN) {
     return res.status(400).send('Ungültiges Datumformat: dueDate')
   }
-  if (Boolean(req.body.complete) === NaN) {
+  if (req.body.complete && Boolean(req.body.complete) === NaN) {
     return res.status(400).send('Ungültiges Format: complete')
   }
-  if (users.includes(req.body.responsible) === NaN) {
+  if (req.body.responsible && users.includes(req.body.responsible) === false) {
     return res.status(400).send('Ungültige Angabe: responsible')
+  }
+  if (!req.body.createdBy){
+    return res.status(400).send('Fehlende Angabe: createdBy')
+  }
+  if (users.includes(req.body.createdBy) === false) {
+    return res.status(400).send('Ungültige Angabe: createdBy')
   }
 
   const todo = {
@@ -155,7 +77,7 @@ app.post('/api/todo', (req, res) => {
     title: req.body.title,
     description: req.body.description,
     dueDate: req.body.dueDate,
-    complete: req.body.compplete,
+    complete: req.body.compplete ? req.body.complete : false,
     responsible: req.body.responsible,
     createdBy: req.body.createdBy,
     createdAt: new Date(),
@@ -165,23 +87,32 @@ app.post('/api/todo', (req, res) => {
 })
 
 app.put('/api/todo/:id', (req, res) => {
-  const todo = todos.find(obj => obj.id === parseInt(req.params.id))
+  const todo = todos.find(obj => obj.id === req.params.id)
   if (!todo) return res.status(404).send('The object with the given ID was not found.')
 
-  if (Date.parse(req.body.dueDate) === NaN) {
+  if (!req.body.title) {
+    return res.status(400).send('Fehlende Angabe: title')
+  }
+  if (req.body.dueDate && Date.parse(req.body.dueDate) === NaN) {
     return res.status(400).send('Ungültiges Datumformat: dueDate')
   }
-  if (Boolean(req.body.complete) === NaN) {
+  if (req.body.complete && Boolean(req.body.complete) === NaN) {
     return res.status(400).send('Ungültiges Format: complete')
   }
-  if (users.includes(req.body.responsible) === NaN) {
+  if (req.body.responsible && users.includes(req.body.responsible) === false) {
     return res.status(400).send('Ungültige Angabe: responsible')
+  }
+  if (!req.body.createdBy){
+    return res.status(400).send('Fehlende Angabe: createdBy')
+  }
+  if (users.includes(req.body.createdBy) === false) {
+    return res.status(400).send('Ungültige Angabe: createdBy')
   }
 
   todo.title = req.body.title
   todo.description = req.body.description
   todo.dueDate = req.body.dueDate
-  todo.complete = req.body.complete
+  todo.complete = req.body.complete ? req.body.complete : false
   todo.responsible = req.body.responsible
   todo.createdBy = req.body.createdBy
   todo.updatedAt = new Date()
@@ -189,34 +120,40 @@ app.put('/api/todo/:id', (req, res) => {
 })
 
 app.delete('/api/todo/:id', (req, res) => {
-  const objectIndex = todos.findIndex(obj => obj.id === parseInt(req.params.id))
-  if (objectIndex === -1) return res.status(404).send('The object with the given ID was not found.')
+  const todoIndex = todos.findIndex(todo => todo.id === req.params.id)
+  if (todoIndex === -1) return res.status(404).send('The object with the given ID was not found.')
 
-  const deletedObject = todos.splice(objectIndex, 1)
+  todos.splice(todoIndex, 1)
   res.json({
-    "message": `Object with id = ${req.params.id}, has been deleted.`
+    "message": `ToDo with id = ${req.params.id}, has been deleted.`
   })
 })
 
 app.patch('/api/todo/:id', (req, res) => {
-  const object = todos.find(obj => obj.id === parseInt(req.params.id))
-  if (!object) return res.status(404).send('The object with the given ID was not found.')
+  const selectedTodo = todos.find(todo => todo.id === req.params.id)
+  if (!selectedTodo) return res.status(404).send('The object with the given ID was not found.')
 
-  if (req.body.name) {
-    object.name = req.body.name
+  if (req.body.title) {
+    selectedTodo.title = req.body.title
   }
-  if (req.body.data) {
-    if (!object.data) {
-      object.data = {}
-    }
-    for (const key in req.body.data) {
-      if (req.body.data.hasOwnProperty(key)) {
-        object.data[key] = req.body.data[key]
-      }
-    }
+  if (req.body.description) {
+    selectedTodo.description = req.body.description
   }
-  object.updatedAt = new Date()
-  res.json(object)
+  if (req.body.dueDate) {
+    selectedTodo.dueDate = req.body.dueDate
+  }
+  if (req.body.complete !== undefined) {
+    selectedTodo.complete = req.body.complete
+  }
+  if (req.body.responsible) {
+    selectedTodo.responsible = req.body.responsible
+  }
+  if (req.body.createdBy) {
+    selectedTodo.createdBy = req.body.createdBy
+  }
+
+  selectedTodo.updatedAt = new Date()
+  res.json(selectedTodo)
 })
 
 app.listen(port, '0.0.0.0', () => {
