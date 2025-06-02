2a9ad5d2d64b4c0f2764b2c25827eae6e00b3a90
Carlos Artiaga <carlesam89@gmail.com>
2025-06-02 16:22:15 +0200

    Journal Update

---
+++ Work/Carlos/Full-Stack/ReactJS/Tutorials/learnReact01/src/App.tsx
@@ -0,0 +1,13 @@
+//https://react.dev/learn/describing-the-ui
+
+export default function Profile() {
+
+  return (
+    <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+    />
+    
+  );
+}
+
