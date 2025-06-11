c35c336d5ca77d47576d4a1300d64b64e51d6aff
masterEmin <masteremin0@gmail.com>
2025-06-12 00:39:35 +0200

    node modules on main pc

--- Work/Mehmet/FullStackm/Projects/Week 17/Tailwind-eminV1/tailwind-v1/src/App.tsx
+++ Work/Mehmet/FullStackm/Projects/Week 17/Tailwind-eminV1/tailwind-v1/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <button className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded shadow-md transition duration-200">
           Speichern
         </button>
+        <h1 className='bg-blue-700 hover:bg-blue-500 text-white font-semibold cursor: pointer'>nah, i'd react</h1>
       </div>
     </>
   )
