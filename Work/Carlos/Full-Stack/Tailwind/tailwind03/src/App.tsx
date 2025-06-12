e551ba09ffb1c76f6830c87bf71c4f17c4f3095a
Carlos Artiaga <carlesam89@gmail.com>
2025-06-12 15:40:39 +0200

    Journal Update

--- Work/Carlos/Full-Stack/Tailwind/tailwind03/src/App.tsx
+++ Work/Carlos/Full-Stack/Tailwind/tailwind03/src/App.tsx
@@ -1,52 +1,49 @@
 
 
-
 export default function App() {
-return (
-<div className="bg-bgcustom font-jetBrains w-screen h-screen flex flex-col">
-<MenuBox />
-<Placeholder title="Main Content" className="w-full flex-1" />
-<Placeholder title="Footer" className="w-full h-16" />
-</div>
-)
+  return (
+    <div className="bg-bgcustom font-jetBrains min-h-screen flex flex-col font-serif">
+      <MenuBox />
+      <MainContent className="w-full flex-1" />
+      <Placeholder title="Footer" className="w-full h-16" />
+    </div>
+  )
 }
 
 function MenuBox() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12">
-    <Placeholder title="De / De" className="w-full h-32" />
-    <KonfigurationsBox className="w-full col-span-1 lg:col-span-8 min-h-32" />
-    <Placeholder title="OSMO Logo" className="w-full min-h-32 col-span-1 lg:col-span-3 lg:row-span-2" />
-    <HolzFarbeBox className="w-full min-w-32 min-h-32 col-span-1 lg:col-span-9" />
-  </div>
+      <KonfigurationsBox className="w-full col-span-1 lg:col-span-9 min-h-32" />
+      <Placeholder title="OSMO Logo" className="w-full min-h-32 col-span-1 lg:col-span-3 lg:row-span-2" />
+      <HolzFarbeBox className="w-full min-w-32 min-h-32 col-span-1 lg:col-span-9" />
+    </div>
+  )
+}
+
+function KonfigurationsBox({ className }: { className?: string }) {
+  return (
+    <div className={`grid grid-cols-6 ${className ?? ''}`}>
+      <Placeholder title="De / De" className="col-span-2" />
+      <Placeholder title="Konfiguratoren"/>
+      <Placeholder title="Farbmusterbestellung"/>
+      <Placeholder title="Verbrauchsrechner"/>
+      <Placeholder title="Mediathek"/>
+    </div>
   )
 }
 
 function HolzFarbeBox({ className }: { className?: string }) {
   return (
-    <div className={`grid grid-cols-10 ${className ?? ''}`}>
-    
-      <Placeholder title="Farbe 1" className="col-span-2" />
-      <Placeholder title="Farbe 2" className="col-span-2" />
-      <Placeholder title="Farbe 3" className="col-span-2" />
-      <Placeholder title="Farbe 4" className="col-span-2" />
-      <Placeholder title="Farbe 5" className="col-span-2" />
-      <Placeholder title="Farbe 6" className="col-span-2" />
-      <Placeholder title="Farbe 7" className="col-span-2" />
-      <Placeholder title="Farbe 8" className="col-span-2" />
-      <Placeholder title="Farbe 9" className="col-span-2" />
-      <Placeholder title="Farbe 10" className="col-span-2" />
-    </div>
-  )
-}   
-
-function KonfigurationsBox({ className }: { className?: string }) {
-  return (
-    <div className={`grid grid-cols-4 ${className ?? ''}`}>
-      <Placeholder title="Konfiguratoren"/>
-      <Placeholder title="Col2"/>
-      <Placeholder title="Col3"/>
-      <Placeholder title="Col4"/>
+    <div className={`grid grid-cols-9 ${className ?? ''}`}>
+      <Placeholder title="Holz" className="col-span-1" />
+      <Placeholder title="Farbe" className="col-span-1" />
+      <Placeholder title="Neue Werkstoffe" className="col-span-1" />
+      <Placeholder title="Referenzen" className="col-span-1" />
+      <Placeholder title="Ideengeber" className="col-span-1" />
+      <Placeholder title="Service" className="col-span-1" />
+      <Placeholder title="Händlersuche" className="col-span-1" />
+      <Placeholder title="Osmo" className="col-span-1" />
+      <Placeholder title="Suchen" className="col-span-1" />
     </div>
   )
 }
@@ -54,14 +51,91 @@ function KonfigurationsBox({ className }: { className?: string }) {
 function Placeholder({ title, className }: { title: string, className?: string}) {
   return (
     <div className={`relative border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
-      <span className="absolute w-full text-center font-bold text-lg text-gray-400">"
-        {title}</span>
-        <svg width="100%" height="100%" className="absolute top-0 left-0">
-        <line x1="0" y1="0" x2="100%" y2="100%" stroke="#bbb" strokeWidth="2" />
-        <line x1="100%" y1="0" x2="0" y2="100%" stroke="#bbb" strokeWidth="2" />
-      </svg>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        {title}
+      </span>
+    </div>
+  )
+}
+
+function MainContent({className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex flex-col h-full ${className ?? ''}`}>
+      <KatalogBox className="w-full flex-1 bg-green" />
+      <ImageBox className="w-full flex-1" />
+      <Produkte className="w-full flex-1" />
+      <Highlights className="w-full flex-1" />
+      <CardMenu className="w-full flex-1" />
+      <Lifecyle  title="Lifecycle" className="w-full flex-1" />
+    </div>
+  )
+}
+
+function Lifecyle({ title, className }: { title: string, className?: string}) {
+  return (
+    <div className={`relative border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        {title}
+      </span>
+    </div>
+  )
+}
+
+function KatalogBox({ className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <h2 className="w-full text-center font-bold text-lg text-gray-400">Unsere Aktuellen Kataloge im Bereich Holz:</h2>
+    </div>
+  )
+}
+
+function ImageBox({ className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        Image Placeholder
+      </span>
+    </div>
+  )
+}
+
+function Produkte({ className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center ${className ?? ''}`}>
+      <ProdukteSection name="Holz" className="w-full" />
+      <ProdukteSection name="Farbe" className="w-full" />
+    </div>
+  )
+}
+
+function ProdukteSection({name, className}: {name:string; className?: string}) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        {name ? name : 'Produkte Section'}
+      </span>
+    </div>
+  )
+}
+
+function Highlights ({ className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        Produkthighlights
+      </span>
+    </div>
+  )
+}
+
+function CardMenu({ className }: { className?: string }) {
+  return (
+    <div className={`border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="w-full text-center font-bold text-lg text-gray-400">
+        Card Menu
+      </span>
     </div>
   )
 
-} 
 
+}
--- Work/Oliver/week-17/tailwind-theme/src/App.tsx
+++ Work/Oliver/week-17/tailwind-theme/src/App.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react"
+import type { JSX } from "react"
 import { Files, Palette } from "lucide-react";
 
 function App() {
