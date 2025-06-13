08c955320535608f94cc4037671299fd7d9eb683
Daniela <pittnerdaniela1@gmail.com>
2025-06-13 09:51:49 +0200

    swiper

--- Work/Daniela/Week 17/osmo-firstTailwind/src/App.tsx
+++ Work/Daniela/Week 17/osmo-firstTailwind/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+//import { useState } from 'react';
+import HeroSlider from './components/HeroSlider';
 import Navbar from './components/Navbar';
 
 
@@ -10,10 +11,27 @@ function App() {
     <>
 
     <Navbar/>
-
+    <Placeholder title="AnnouncementBar" className="w-screen h-70"/>
+    <HeroSlider/>
+    
 
     </>
   )
 }
 
 export default App
+
+
+function Placeholder({ title, className }: { title: string, className?: string }) {
+  return (
+    <div className={`relative border-2 border-dashed border-gray-300 flex items-center justify-center ${className ?? ''}`}>
+      <span className="absolute top-2 left-0 w-full text-center font-bold text-lg text-gray-400">
+        {title}
+      </span>
+      <svg width="100%" height="100%" className="absolute top-0 left-0">
+        <line x1="0" y1="0" x2="100%" y2="100%" stroke="#bbb" strokeWidth="2" />
+        <line x1="100%" y1="0" x2="0" y2="100%" stroke="#bbb" strokeWidth="2" />
+      </svg>
+    </div>
+  );
+}
