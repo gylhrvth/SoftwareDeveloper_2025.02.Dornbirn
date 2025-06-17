7e2174e5271fa80d8a9bfc05948b7037799e8b58
Carlos Artiaga <carlesam89@gmail.com>
2025-06-17 15:33:02 +0200

    Journal Update

---
+++ Work/Carlos/Full-Stack/NextJS/mondial-next-js-tutorial-v01/src/app/country/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+
+// src/app/country/components/SearchBar.tsx
+
+"use client";
+
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useRouter, useSearchParams } from 'next/navigation';
+
