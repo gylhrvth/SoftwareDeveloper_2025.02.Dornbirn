04184858fb0043390d0d615c16cfada7d30c85ea
Emin <masteremin0@gmail.com>
2025-06-17 15:05:01 +0200

    slug country

--- Work/Mehmet/FullStackm/Projects/Week 18 next.js/next-V/next-V2-mondial/mondial/src/app/country/page.tsx
+++ Work/Mehmet/FullStackm/Projects/Week 18 next.js/next-V/next-V2-mondial/mondial/src/app/country/page.tsx
@@ -1,27 +1,33 @@
-import { getAllCountries } from '@/lib/db';
-import { type Country } from '@/lib/db';
+// src/app/country/page.tsx
+
+import Link from "next/link";
+import { getAllCountries } from "@/lib/db";
+import type { Country } from "@/lib/db";
 
 export default async function Country() {
-  const countries: Country[] = await getAllCountries();
-  //console.log(countries);
+  const countries = await getAllCountries();
 
   return (
     <div>
       <h1 className="text-4xl font-bold text-center my-8">Country Page</h1>
-      <div className="flex gap-4 flex-wrap">
-        {countries.map((country: any, index: number) => (
-            <div
-            key={index}
-            className="w-72 border border-gray-300 p-4 rounded shadow-sm bg-white transition-transform duration-200 hover:scale-105 hover:shadow-lg  text-black"
-            >
-            <h3 className="text-lg font-semibold mb-2">{country.Name}</h3>
-            <h4 className=' mb-2'>Captal: <span className='text-red-700'> {country.Capital} </span></h4>
-            <p className="text-sm text-green-600">Population: {country.Population}</p>
-            <p className="text-sm text-amber-900">Area: {country.Area} km²</p>
+      <div className="flex gap-4 flex-wrap justify-center">
+        {countries.map((country) => (
+          <Link key={country.Code} href={`/country/${country.Code}`}>
+            <div className="w-72 border border-gray-300 p-4 rounded shadow-sm bg-white transition-transform duration-200 hover:scale-105 hover:shadow-lg text-black cursor-pointer">
+              <h3 className="text-lg font-semibold mb-2">{country.Name}</h3>
+              <h4 className="mb-2">
+                Capital: <span className="text-red-700">{country.Capital}</span>
+              </h4>
+              <p className="text-sm text-green-600">
+                Population: {country.Population.toLocaleString()}
+              </p>
+              <p className="text-sm text-amber-900">
+                Area: {country.Area.toLocaleString()} km²
+              </p>
             </div>
+          </Link>
         ))}
       </div>
-      <p>This is the country page content.</p>
     </div>
   );
 }
