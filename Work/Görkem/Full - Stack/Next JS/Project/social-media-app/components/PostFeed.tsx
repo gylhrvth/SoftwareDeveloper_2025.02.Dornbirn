ff61e662bee424be4b4658553c13473bff631d73
Görkem Cinar <grcinar@outlook.com>
2025-06-18 22:04:55 +0200

    Add some style, postfeed and postinput

--- Work/Görkem/Full - Stack/Next JS/Project/social-media-app/components/PostFeed.tsx
+++ Work/Görkem/Full - Stack/Next JS/Project/social-media-app/components/PostFeed.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PostInput from './PostInput';
 
 export default function PostFeed() {
     return (
-        <div className='flex-grow border border-black'>PostFeed</div>
+        <div className='flex-grow border border-black
+        max-w-2xl
+        '>
+            <div className='py-4 px-3 text-lg sm:text-xl sticky top-0
+            z-50 bg-white bg-opacity-80 background-blur-sm font-bold
+            '>
+                Home
+            </div>
+            <PostInput />
+        </div>
     )
 }
